bc805c362c447404c75b2052adb44591873058ba
Сергей <seirosso1@gmail.com>
2020-10-23 20:26:59 +0300

    Таблица умножения!

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class StudentInfo {
+
+    public static void main(String[] args){
+
+    System.out.println("Сержик супер программист!");
+        System.out.println("03.09.1984 г");
+    }
+}
