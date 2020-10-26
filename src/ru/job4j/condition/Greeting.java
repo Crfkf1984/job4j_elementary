fd9657697845b293fe679c4b7cbfa82181b3dd8b
Сергей <seirosso1@gmail.com>
2020-10-26 19:46:59 +0300

    Строка изменение !!!

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -5,8 +5,11 @@ public class Greeting {
      public static void main(String[] args) {
 
          String idea = "I like Java!";
+         System.out.println(idea);
+         String slovo = idea + "But I am a newbie.";
          int year = 2020;
-         System.out.println(idea + year + " But I am a newbie.");
+         String slovo1 = slovo + year;
+         System.out.println(slovo1);
 
 
      }
