6876c6e32ad5438caf90af4f6a0694af77732241
Сергей <seirosso1@gmail.com>
2020-10-31 12:44:17 +0300

    Тест на стороны треугольника!!!

--- src/ru/job4j/condition/TrgArea.java
+++ src/ru/job4j/condition/TrgArea.java
@@ -2,11 +2,9 @@ package ru.job4j.condition;
 
 public class TrgArea {
 
-    public static double area(int p,int k) {
+    public static double area(double a,double b,double c) {
 
-        int  a = 2;
-        int b = 2;
-        int c = 2;
+        double p = (a + b + c) / 2;
         double s = p * (p - a) * (p - b) * (p - c);
         double rsl = Math.sqrt(s);
         return rsl;
@@ -14,9 +12,8 @@ public class TrgArea {
 
     public static void main(String[] args) {
 
-        int p = 4;
-        int k = 2;
-        double rsl = TrgArea.area(p,k);
+
+        double rsl = TrgArea.area(2,2,2);
         System.out.println("area (2, 2, 2) = " + rsl);
     }
 }
