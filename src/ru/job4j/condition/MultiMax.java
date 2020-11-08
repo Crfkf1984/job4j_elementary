4b8fab41b3666845c04e23b28759ad464d19fc5e
Сергей <seirosso1@gmail.com>
2020-11-08 14:13:58 +0300

    Вложенный оператор!!!

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -4,29 +4,16 @@ public class MultiMax {
 
     public static int max(int first, int second, int third) {
         int result = first;
+
         if (first > second) {
+
             int rsl = first > second ? first : second;
             rsl = rsl > third ? rsl : third;
 
             return rsl;
 
         }
-        if (second > first) {
 
-            int rsl = second > first ? second : first;
-            rsl = rsl > third ? rsl : third;
-
-            return rsl;
-
-            }
-        if (third > first) {
-
-            int rsl = third > first ? third : first;
-            rsl = rsl > second ? rsl : second;
-
-            return rsl;
-        }
-
-        return result;
+            return result;
     }
 }
