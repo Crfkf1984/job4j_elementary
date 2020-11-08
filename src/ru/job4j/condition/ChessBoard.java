9ff26892ef33f6e98fbf7e7c15d67371b4120942
Сергей <seirosso1@gmail.com>
2020-11-08 16:55:06 +0300

    Фигура слон!

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -8,7 +8,7 @@ public class ChessBoard {
         if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
         rsl = Math.abs(x2 - x1);
 
-        return rsl != 0 ? Math.abs(y2 - y1) : 0;
+        return rsl;
 
         }
 
