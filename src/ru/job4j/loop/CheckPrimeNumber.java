db68400c128924643df9a16ee50e5cf152541ab1
Сергей <seirosso1@gmail.com>
2020-11-19 21:01:46 +0300

    Простое число!!!

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -4,18 +4,21 @@ public class CheckPrimeNumber {
 
     public static boolean check(int number) {
 
+        if (number == 1) {
+
+            return false;
+        }
+
         for (int index = 2; index < number; index++) {
 
             if (number % index == 0) {
 
-                    return false;
+                return false;
 
-            } else {
+            }
 
-                    return true;
-                }
         }
 
-        return false;
+            return true;
     }
 }
