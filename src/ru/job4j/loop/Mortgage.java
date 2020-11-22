d1f31a3eb141326ebd01b2fbbe16cfa177260999
Сергей <seirosso1@gmail.com>
2020-11-22 16:39:21 +0300

    Ипотека !!!

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -2,22 +2,21 @@ package ru.job4j.loop;
 
 public class Mortgage {
 
-    public static int year(int amount, int salary, double percent) {
+    public static int year(double amount, int salary, double percent) {
 
         int year = 0;
-        double wsego =  amount + amount * (percent / 100);
+        amount = amount + amount * (percent / 100);
 
-        while (salary <= wsego) {
+        while (salary <= amount) {
 
-           double ostatok = wsego - salary;
-           double rsl = ostatok + ostatok * (percent / 100);
-           wsego = rsl;
+            amount -= salary;
+           amount += amount * (percent / 100);
 
            year += 1;
 
         }
 
-        while (salary > wsego) {
+        while (salary > amount) {
 
              year += 1;
             return year;
@@ -26,3 +25,13 @@ public class Mortgage {
         return year;
     }
 }
+
+   // Цикл сделайте такой:
+
+       // while (amount > 0) {
+
+        //а значения будете записывать в саму же переменную amount , т.е. постоянно ее переназначать
+
+       // amount += amount * (percent / 100);
+
+       // amount -= salary;
