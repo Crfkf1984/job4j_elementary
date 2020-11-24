d822229398c53065539ace08f014abc8b8f08672
Сергей <seirosso1@gmail.com>
2020-11-24 08:16:26 +0300

    Генерация в IDEA!!!

--- src/ru/job4j/live/Loop.java
+++ src/ru/job4j/live/Loop.java
@@ -5,11 +5,8 @@ public class Loop {
     public static void main(String[] args) {
 
         int size = 10;
-
         for (int i = 0; i <= size; i++) {
-
             System.out.println(i);
-
         }
     }
 }
