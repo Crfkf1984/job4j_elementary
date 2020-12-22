a90290be16c40d72e734277abac4ffeb88bce7fc
Сергей <seirosso1@gmail.com>
2020-12-22 21:07:51 +0300

    Слияние массива с сортировкой!

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -3,23 +3,21 @@ package ru.job4j.array;
 public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        for (int i = 0; i <= left.length - 1; i++) {
-            rsl[i] = left[i];
-        }
-
-        for (int index = 0; index <= right.length - 1; index++) {
-            rsl[index + left.length] = right[index];
-        }
-        for (int j = 0; j < rsl.length - 1; j++) {
-            for (int a = 0; a < rsl.length - 2; a++) {
-                if (rsl[a] > rsl[a + 1]) {
-                    int temp = rsl[a];
-                    rsl[a] = rsl[a + 1];
-                    rsl[a + 1] = temp;
-
-                }
+        int positionleft = 0;
+        int positionright = 0;
+        for (int i = 0; i < left.length + right.length; i++) {
+            if (i >= left.length) {
+                rsl[i] = right[i - positionright];
+                //positionleft++;
+            } else if (left[i - positionleft] < right[i - positionright]) {
+                rsl[i] = left[i - positionleft];
+                positionright++;
+            } else {
+                rsl[i] = right[i - positionright];
+                positionleft++;
             }
         }
+
         return rsl;
     }
 
