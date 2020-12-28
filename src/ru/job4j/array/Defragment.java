1b9ce110b31ef0fb02b6665cd7f901489ce8fb69
Сергей <seirosso1@gmail.com>
2020-12-28 21:02:41 +0300

    1. Чтение кода и базовые ошибки

--- src/ru/job4j/array/Defragment.java
+++ src/ru/job4j/array/Defragment.java
@@ -17,8 +17,7 @@ public class Defragment {
             System.out.print(array[index] + " ");
         }
            return array;
-}
-    public static void main(String[] args) {
+}       public static void main(String[] args) {
         String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
         String[] compressed = compress(input);
         System.out.println();
--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -2,7 +2,6 @@ package ru.job4j.array;
 
 public class Merge {
     public static int[] merge(int[] left, int[] right) {
-
         int[] rsl = new int[left.length + right.length];
         int leftPoint = 0;
         int rightPoint = 0;
